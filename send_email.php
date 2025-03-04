83396989df20fa8551905bc3f548bf137853db4e
Karen Zheng <karen.zheng0707@gmail.com>
2025-03-04 01:14:44 -0500

    php-mailer

--- send_email.php
+++ send_email.php
@@ -1,24 +1,35 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $to = "kzheng92@gatech.edu"; // your email address
-    $subject = "New Contact Form Submission";
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
 
-    // Compose the email content
-    $message_content = "Name: $name\n";
-    $message_content .= "Email: $email\n";
-    $message_content .= "Message: $message";
+$name = $_POST["name"];
+$email = $_POST["email"];
+$subject = $_POST["subject"];
+$message = $_POST["message"];
 
-    // Set email headers
-    $headers = "From: $email";
+require "vendor/autoload.php";
 
-    // Send email
-    if(mail($to, $subject, $message_content, $headers)) {
-        echo "Message sent successfully!";
-    } else {
-        echo "Failed to send message.";
-    }
-}
-?>
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+$mail = new PHPMailer(true);
+
+// $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+
+$mail->isSMTP();
+$mail->SMTPAuth = true;
+
+$mail->Host = "smtp.example.com";
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+$mail->Port = 587;
+
+$mail->Username = "you@example.com";
+$mail->Password = "password";
+
+$mail->setFrom($email, $name);
+$mail->addAddress("dave@example.com", "Dave");
+
+$mail->Subject = $subject;
+$mail->Body = $message;
+
+$mail->send();
+
+header("Location: sent.html");
